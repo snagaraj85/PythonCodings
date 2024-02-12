3a60ea0fc498d42c8b694f1ede7fe8a9d8ea8106
Nagarajan S <69498491+snagaraj85@users.noreply.github.com>
2024-02-12 16:56:28 +0530

    Add files via upload
    DateTime-v1

--- DateTime.py
+++ DateTime.py
@@ -4,10 +4,11 @@ Course: Python-AI
 FileName: DateTime
 Batch: 3.30 PM - 5.30 PM
 """
+import datetime
 from datetime import *
-today = date.today()
+today = datetime.today()
 time=datetime.now()
-print("Today's date is", today)
+print("Today's date is", today.date())
 
 print("Current year:", today.year)
 print("Current month:", today.month)
@@ -21,3 +22,5 @@ print("Current Time: ",t)
 print("Current Hour: ",time.strftime("%H"))
 print("Current Minute: ",time.strftime("%M"))
 print("Current Second: ",time.strftime("%S"))
+
+print("Current Date/Time: ",today.strftime("%d-%b-%Y %H:%M:%S"))
