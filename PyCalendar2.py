ff5c44b6dd8cc24d53519fa717ae9bfed68e702a
Nagarajan S <69498491+snagaraj85@users.noreply.github.com>
2024-02-12 17:30:39 +0530

    Add files via upload
    Calendar2

---
+++ PyCalendar2.py
@@ -0,0 +1,10 @@
+# Python code to demonstrate the working of
+# monthrange() and prcal()
+
+# importing calendar module for calendar operations
+import calendar
+
+# using monthrange() to print start week day and
+# number of month
+print("The start week day and no. of days of month : ", end="")
+print(calendar.monthrange(2024,3))
