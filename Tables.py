74857a7c3303779f5266a748eaad0b22c8b985fe
Nagarajan S <69498491+snagaraj85@users.noreply.github.com>
2024-02-19 14:35:55 +0530

    Add files via upload

---
+++ Tables.py
@@ -0,0 +1,11 @@
+s1=2
+s2=4
+for i in range(s1,s2+1):
+    for j in range(1,11):
+        print(i,"X",j,"=",j*i)
+
+s1=4
+s2=2
+for i in range(s2,s1+1):
+    for j in range(1,11):
+        print(i,"X",j,"=",j*i)
